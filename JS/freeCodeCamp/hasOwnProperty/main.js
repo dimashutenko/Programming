edcd4c473d3efdb2bf1260639a26ac16cb99460d
dimashutenko <58172685+dimashutenko@users.noreply.github.com>
2021-01-04 14:19:37 +0200

    freecodecamp

---
+++ JS/freeCodeCamp/hasOwnProperty/main.js
@@ -0,0 +1,44 @@
+jQuery(document).ready(function ($) {
+    var collection = {
+        2548: {
+          albumTitle: 'Slippery When Wet',
+          artist: 'Bon Jovi',
+          tracks: ['Let It Rock', 'You Give Love a Bad Name']
+        },
+        2468: {
+          albumTitle: '1999',
+          artist: 'Prince',
+          tracks: ['1999', 'Little Red Corvette']
+        },
+        1245: {
+          artist: 'Robert Palmer',
+          tracks: []
+        },
+        5439: {
+          albumTitle: 'ABBA Gold'
+        }
+      };
+      
+      // Only change code below this line
+      function updateRecords(object, id, prop, value) {
+        if(prop !== 'tracks' && value !== ''){
+          object[id][prop] = value;
+        }
+        else if(prop == 'tracks' && object[id].hasOwnProperty('tracks') == false){
+          object[id]['tracks'] = [value];
+        }
+        else if (prop==='tracks' && value !== '' ){
+          object[id][prop].push(value);
+        }
+        else if (value===""){
+          delete object[id][prop];
+        }
+        console.log(object[id]);
+        return object; 
+      }
+      
+      updateRecords(collection, 5439, "tracks", "Take a Chance on Me");
+      
+
+});    
+
