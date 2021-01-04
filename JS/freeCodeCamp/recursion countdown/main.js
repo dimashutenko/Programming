1019959a75f019619f3ce601d8e0b71986fffac5
dimashutenko <58172685+dimashutenko@users.noreply.github.com>
2021-01-04 19:47:07 +0200

    0

--- JS/freeCodeCamp/recursion countdown/main.js
+++ JS/freeCodeCamp/recursion countdown/main.js
@@ -1,24 +1,21 @@
 jQuery(document).ready(function ($) {
   
+  
   countdown = (n) => {
     if (n < 1){
       return []
     } else {
-      setTimeout(function(){
         const arr = countdown(n-1);
         arr.unshift(n);
         el.innerHTML = n;
         return arr;
-      }, 1000);
-      
-      
     }
   }
 
   
-  var el = document.getElementById('h1_id');
+  var el = document.getElementById('h1_id'); 
   var v = countdown(10);
-  console.log(v);
+  el.innerHTML = v;
   
 
 });    
