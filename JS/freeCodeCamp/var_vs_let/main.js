7007cd65c4eeadee39ee5b594e584b348664cf9f
dimashutenko <58172685+dimashutenko@users.noreply.github.com>
2021-01-05 20:51:52 +0200

    0

---
+++ JS/freeCodeCamp/var_vs_let/main.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function ($) {
+    
+  let printNumTwo; debugger
+  for (let i = 0; i < 3; i++) {
+    if (i === 2) {
+      printNumTwo = function() {
+        return i;
+      };
+    }
+  }
+  console.log(printNumTwo());
+  // returns 2
+  console.log(i);
+  // returns "i is not defined"
+
+});    
+
