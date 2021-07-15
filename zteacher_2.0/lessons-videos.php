9be70bb5d5e580dc72e84ceacae9bb5f3512d37f
dimashutenko <58172685+dimashutenko@users.noreply.github.com>
2021-07-15 13:19:55 +0300

    2

--- zteacher_2.0/lessons-videos.php
+++ zteacher_2.0/lessons-videos.php
@@ -4,14 +4,14 @@
     </h2>
     <div class="container">
         <div class="row">
-            <div class="videos-gallery">
+            <div class="videos-gallery children-videos-gallery">
                 <a class="venobox" data-autoplay="true" data-vbtype="video" data-gall="desktopVideoGallery" href="https://youtu.be/U8iV7-ryWTM">
                   <div class="gallery-video"><i class="bi bi-youtube"></i></div> <!-- 1 -->
                 </a>
                 <a class="venobox" data-autoplay="true" data-vbtype="video" data-gall="desktopVideoGallery" href="https://youtu.be/EPrfAAcATpo">
                   <div class="gallery-video"><i class="bi bi-youtube"></i></div> <!-- 2 -->
                 </a>
-                <a class="venobox" data-autoplay="true" data-vbtype="video" data-gall="desktopVideoGallery" href="https://youtu.be/rBmwiuFCKDc">
+                <a class="venobox" data-autoplay="true" data-vbtype="video" data-gall="desktopVideoGallery" href="">
                   <div class="gallery-video"><i class="bi bi-youtube"></i></div> <!-- 3 -->
                 </a>
                 <a class="venobox" data-autoplay="true" data-vbtype="video" data-gall="desktopVideoGallery" href="https://youtu.be/Q1HJWz0ZPzA">
@@ -23,53 +23,13 @@
                 <a class="venobox" data-autoplay="true" data-vbtype="video" data-gall="desktopVideoGallery" href="https://youtu.be/VEAnSCka81c">
                   <div class="gallery-video"><i class="bi bi-youtube"></i></div> <!-- 6 -->
                 </a>
-                <a class="venobox" data-autoplay="true" data-vbtype="video" data-gall="desktopVideoGallery" href="https://youtu.be/vFX2Ynnff38">
+                <a class="venobox" data-autoplay="true" data-vbtype="video" data-gall="desktopVideoGallery" href="">
                   <div class="gallery-video"><i class="bi bi-youtube"></i></div> <!-- 7 -->
                 </a>
-              </div>
-        </div>
-        <div class="row">
-            <div class="col-12 col-md-4">
-                <div class="lesson-video-box">
-                    <h2 class="lesson-video-title">video #1</h2>
-                    <div class="lesson-video-player"></div>
-                    <div class="lesson-video-play-btn"></div>
-                </div>
+                <a class="venobox" data-autoplay="true" data-vbtype="video" data-gall="desktopVideoGallery" href="">
+                  <div class="gallery-video"><i class="bi bi-youtube"></i></div> <!-- 8 -->
+                </a>
             </div>
-            <div class="col-12 col-md-4"><div class="lesson-video-box">
-                <h3 class="lesson-video-title">video #2</h3>
-                <div class="lesson-video-player"></div>
-                <div class="lesson-video-play-btn"></div>
-            </div></div>
-            <div class="col-12 col-md-4"><div class="lesson-video-box">
-                <h3 class="lesson-video-title">video #3</h3>
-                <div class="lesson-video-player"></div>
-                <div class="lesson-video-play-btn"></div>
-            </div></div>
-            <div class="col-12 col-md-4"><div class="lesson-video-box">
-                <h3 class="lesson-video-title">video #4</h3>
-                <div class="lesson-video-player"></div>
-            </div></div>
-            <div class="col-12 col-md-4"><div class="lesson-video-box">
-                <h3 class="lesson-video-title">video #5</h3>
-                <div class="lesson-video-player"></div>
-            </div></div>
-            <div class="col-12 col-md-4"><div class="lesson-video-box">
-                <h3 class="lesson-video-title">video #6</h3>
-                <div class="lesson-video-player"></div>
-            </div></div>
-            <div class="col-12 col-md-4"><div class="lesson-video-box">
-                <h3 class="lesson-video-title">video #7</h3>
-                <div class="lesson-video-player"></div>
-            </div></div>
-            <div class="col-12 col-md-4"><div class="lesson-video-box">
-                <h3 class="lesson-video-title">video #8</h3>
-                <div class="lesson-video-player"></div>
-            </div></div>
-            <div class="col-12 col-md-4"><div class="lesson-video-box">
-                <h3 class="lesson-video-title">video #9</h3>
-                <div class="lesson-video-player"></div>
-            </div></div>
         </div>
     </div>
     <h2 class="headline_700 text-center body-bg">
@@ -77,51 +37,32 @@
     </h2>
     <div class="container">
         <div class="row">
-            <div class="col-12 col-md-4">
-                <div class="lesson-video-box">
-                    <div class="lesson-video-player"></div>
-                    <div class="lesson-video-play-btn"></div>
-                    <a href="https://images.pexels.com/photos/62307/air-bubbles-diving-underwater-blow-62307.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" class="fancybox" rel="ligthbox">
-                        <img  src="https://images.pexels.com/photos/62307/air-bubbles-diving-underwater-blow-62307.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" class="img-fluid "  alt="">
-                    </a>
-                </div>
-            </div>
-            <div class="col-12 col-md-4"><div class="lesson-video-box">
-                <div class="lesson-video-player"></div>
-                <div class="lesson-video-play-btn"></div>
-                <a href="https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"  class="fancybox" rel="ligthbox">
-                    <img  src="https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" class="img-fluid"  alt="">
+            <div class="videos-gallery older-videos-gallery">
+                <a class="venobox" data-autoplay="true" data-vbtype="video" data-gall="VideoGallery-older" href="">
+                  <div class="gallery-video"><i class="bi bi-youtube"></i></div> <!-- 1 -->
                 </a>
-            </div></div>
-            <div class="col-12 col-md-4"><div class="lesson-video-box">
-                <h3 class="lesson-video-title">video #3</h3>
-                <div class="lesson-video-player"></div>
-                <div class="lesson-video-play-btn"></div>
-            </div></div>
-            <div class="col-12 col-md-4"><div class="lesson-video-box">
-                <h3 class="lesson-video-title">video #4</h3>
-                <div class="lesson-video-player"></div>
-            </div></div>
-            <div class="col-12 col-md-4"><div class="lesson-video-box">
-                <h3 class="lesson-video-title">video #5</h3>
-                <div class="lesson-video-player"></div>
-            </div></div>
-            <div class="col-12 col-md-4"><div class="lesson-video-box">
-                <h3 class="lesson-video-title">video #6</h3>
-                <div class="lesson-video-player"></div>
-            </div></div>
-            <div class="col-12 col-md-4"><div class="lesson-video-box">
-                <h3 class="lesson-video-title">video #7</h3>
-                <div class="lesson-video-player"></div>
-            </div></div>
-            <div class="col-12 col-md-4"><div class="lesson-video-box">
-                <h3 class="lesson-video-title">video #8</h3>
-                <div class="lesson-video-player"></div>
-            </div></div>
-            <div class="col-12 col-md-4"><div class="lesson-video-box">
-                <h3 class="lesson-video-title">video #9</h3>
-                <div class="lesson-video-player"></div>
-            </div></div>
+                <a class="venobox" data-autoplay="true" data-vbtype="video" data-gall="VideoGallery-older" href="">
+                  <div class="gallery-video"><i class="bi bi-youtube"></i></div> <!-- 2 -->
+                </a>
+                <a class="venobox" data-autoplay="true" data-vbtype="video" data-gall="VideoGallery-older" href="https://youtu.be/rBmwiuFCKDc">
+                  <div class="gallery-video"><i class="bi bi-youtube"></i></div> <!-- 3 -->
+                </a>
+                <a class="venobox" data-autoplay="true" data-vbtype="video" data-gall="VideoGallery-older" href="">
+                  <div class="gallery-video"><i class="bi bi-youtube"></i></div> <!-- 4 -->
+                </a>
+                <a class="venobox" data-autoplay="true" data-vbtype="video" data-gall="VideoGallery-older" href="">
+                  <div class="gallery-video"><i class="bi bi-youtube"></i></div> <!-- 5 -->
+                </a>
+                <a class="venobox" data-autoplay="true" data-vbtype="video" data-gall="VideoGallery-older" href="">
+                  <div class="gallery-video"><i class="bi bi-youtube"></i></div> <!-- 6 -->
+                </a>
+                <a class="venobox" data-autoplay="true" data-vbtype="video" data-gall="VideoGallery-older" href="https://youtu.be/vFX2Ynnff38">
+                  <div class="gallery-video"><i class="bi bi-youtube"></i></div> <!-- 7 -->
+                </a>
+                <a class="venobox" data-autoplay="true" data-vbtype="video" data-gall="VideoGallery-older" href="">
+                  <div class="gallery-video"><i class="bi bi-youtube"></i></div> <!-- 8 -->
+                </a>
+            </div>
         </div>
     </div>
 </div>
