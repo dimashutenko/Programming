b25267a4b532b60faf88e9476899d01794372a68
Dima Shutenko <dima.shutenko.official@gmail.com>
2021-08-08 11:27:14 +0300

    modal

--- zteacher_2.0/trial_lesson_modal.php
+++ zteacher_2.0/trial_lesson_modal.php
@@ -2,7 +2,7 @@
   <div class="modal-dialog"> <!-- role="document" -->
     <div class="modal-content">
       <div class="modal-header">
-        <h4 class="modal-title">Запис на пробний урок</h4>
+        <h4 class="modal-title">Запис на пробне заняття</h4>
         <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -11,26 +11,37 @@
         <form role="form" class="needs-validation" novalidate method="POST" action="./php/mail.php" id="trial-lesson-form">
           <input type="hidden" name="_token" value="">
           <div class="form-group">
-              <!-- <label class="control-label">Ім'я</label> -->
-              <div>
-                <input type="text" class="form-control" id="form-input-user-name" name="user_name" placeholder="Ім'я *" required="true">
-                <div class="invalid-feedback">Будь ласка, введіть ваше ім'я</div>
-              </div>
+            <div>
+              <label for="form-input-user-name" class="">Ім'я *</label>
+              <input type="text" class="form-control" id="form-input-user-name" name="user_name" placeholder="" required="true">
+              <div class="invalid-feedback">Будь ласка, введіть ваше ім'я</div>
+            </div>
           </div>
-          <div class="w-100"> 
-            <select id="choices-multiple-remove-button" placeholder="Виберіть, що вас зацікавило" name="products-select[]" multiple>
-              <option value="Загальна англійська">Загальна англійська</option>
-              <option value="Розмовна англійська">Розмовна англійська</option>
-              <option value="Підготовка до іспитів">Підготовка до іспитів</option>
-              <option value="Онлайн марафон по серіалу Dash and Lily">Онлайн марафон по серіалу "Dash and Lily"</option>
-              <option value="Курс по серіалу Young Sheldon">Курс по серіалу "Young Sheldon"</option>
-              <option value="Вебінари від засновниці школи Nina Zelinska для вчителів">Вебінари від засновниці школи Nina Zelinska для вчителів</option>
-              <option value="Курс Подорожі з легкістю">Курс "Подорожі з легкістю"</option>
-              <option value="Курс по серіалу Dash and Lily">Курс по серіалу "Dash and Lily"</option>
-              <option value="Уроки для юних IT-шників">Уроки для юних IT-шників</option>
-              <option value="Гарячі дискусії">Гарячі дискусії</option>
-              <option value="Курс з математики та логіки для дітей">Курс з математики та логіки для дітей</option>
-            </select>
+          <div class="form-group">
+            <div>
+              <label for="form-input-user-phone" class="">Телефон *</label>
+              <input type="text" class="form-control" id="form-input-user-phone" name="user_name" placeholder="" required="true">
+              <div class="invalid-feedback">Будь ласка, введіть ваш телефон</div>
+            </div>
+          </div>
+          <div class="form-group">
+            <div>
+              <label for="form-input-user-course" class="">Курс</label>
+              <select id="choices-multiple-remove-button" placeholder="" name="products-select[]" multiple>
+                <option value="Загальна англійська">Загальна англійська</option>
+                <option value="Розмовна англійська">Розмовна англійська</option>
+                <option value="Підготовка до іспитів">Підготовка до іспитів</option>
+                <option value="Онлайн марафон по серіалу Dash and Lily">Онлайн марафон по серіалу "Dash and Lily"</option>
+                <option value="Курс по серіалу Young Sheldon">Курс по серіалу "Young Sheldon"</option>
+                <option value="Вебінари від засновниці школи Nina Zelinska для вчителів">Вебінари від засновниці школи Nina Zelinska для вчителів</option>
+                <option value="Курс Подорожі з легкістю">Курс "Подорожі з легкістю"</option>
+                <option value="Курс по серіалу Dash and Lily">Курс по серіалу "Dash and Lily"</option>
+                <option value="Уроки для юних IT-шників">Уроки для юних IT-шників</option>
+                <option value="Гарячі дискусії">Гарячі дискусії</option>
+                <option value="Курс з математики та логіки для дітей">Курс з математики та логіки для дітей</option>
+              </select>
+              <div class="invalid-feedback"></div>
+            </div>
           </div>
           
           <h5 class="mb-2 mt-3">Як з вами зв'язатись? *</h5>
@@ -44,10 +55,6 @@
                         </label>
                         <label class="btn btn-outline-secondary mr-1">
                           <input type="radio" name="radio_options" class="radio_communication" id="radio_whatsapp" onclick="display_user_choice(this.id)"> <span class="d-none d-md-inline">WhatsApp</span> <i class="d-inline d-md-none fab fa-whatsapp"></i>
-                        </label>
-                        <label class="btn btn-outline-secondary mr-1">
-                          <input type="radio" name="radio_options" class="radio_communication" id="radio_phone" onclick="display_user_choice(this.id)"> 
-                          <i class="fas fa-phone"></i></button>
                         </label>
                         <label class="btn btn-outline-secondary">
                           <input type="radio" name="radio_options" class="radio_communication" id="radio_email" onclick="display_user_choice(this.id)"> 
@@ -64,14 +71,10 @@
                       <input type="text" class="form-control input-for-user-contact d-none" id="user_telegram" name="user_telegram" placeholder="Ваш номер телефону або акаунт в Telegram *">
                       <input type="text" class="form-control input-for-user-contact d-none" id="user_viber" name="user_viber" placeholder="Ваш номер телефону або акаунт у Viber *">
                       <input type="text" class="form-control input-for-user-contact d-none" id="user_whatsapp" name="user_whatsapp" placeholder="Ваш номер телефону або акаунт в WhatsApp *">
-                      <input type="phone" class="form-control input-for-user-contact d-none" id="user_phone" name="user_phone" placeholder="Ваш номер телефону *">
                       <input type="email" class="form-control input-for-user-contact d-none" id="user_email" name="user_email" placeholder="Ваш email *">
                       <div class="invalid-feedback"> Введіть контактні дані, будь ласка </div>
                     </div>
-                    <!-- <div class="checkbox mt-2">
-                      <input type="checkbox" class="input" id="customCheck1" value="Yes" name="user_write">
-                      <label class="label" for="customCheck1">Краще написати ніж подзвонити</label>
-                    </div> -->
+
                     <div class="checkbox mt-1">
                       <input type="checkbox" class="input" id="user_agreement" checked="true" required="true" name="user_write">
                       <label class="label mb-0" for="user_agreement">Згода на обробку персональних даних *</label>
