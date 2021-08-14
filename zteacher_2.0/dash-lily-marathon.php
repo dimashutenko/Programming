c6f59a91a58b9f46a100563c0e8678e4f0ba43ab
Dima Shutenko <dima.shutenko.official@gmail.com>
2021-08-14 13:15:05 +0300

    it

--- zteacher_2.0/dash-lily-marathon.php
+++ zteacher_2.0/dash-lily-marathon.php
@@ -12,7 +12,7 @@
     <meta property="og:type" content="article">
     <meta property="og:image" content="img/favicon.png">
     <meta property="og:site_name" content="Zteacher Language School">
-    <title>Travel English - Zteacher Language School</title>
+    <title>Марафон Dash&Lily - Zteacher Language School</title>
     <body>
 
       <?php include ('trial_lesson_modal.php'); // запись на пробный modal ?>
@@ -25,21 +25,37 @@
               <div class="col-md-6">
                 <div class="course-right d-flex">
                   <h2 class="course-page-title">
-                    Курс Подорожі З Легкістю
+                    Онлайн марафон по серіалу "Dash and Lily"
                   </h2>
                   <div class="mobile-course-img course-left-travel"></div>
-                  <p class="course-description">Позбавимо вас від страху непорозумінь під час подорожей за 10 занять. Розбираємо та тренуємо найбільш важливі теми та знайомимосьз визначними місцями.</p>
-                  <p class="course-description">Судячи з інстаграм, перші ластівки полетіли до вже приймаючих готелів, в межах України. Невдовзі найсміливіші відправляться і в закордонні поїздки. У нас, для плануючих відпочивати із задоволенням, та отримати максимум від подорожей, нереально цікава пропозиція.</p>
-                  <p class="course-description">10 уроків, тривалістю 1.15 год. <br>Найцікавіші факти, про місця 10ти міст світу</p>
-                  <p class="course-description">10 тем, якими найважливіше оволодіти для вільного, впевненого спілкування під час подорожей пропрацювання лексики, конструкцій, сталих виразів. Незважаючи на курс, індивідуальний підхід до кожного студента зберігається (адаптуємо урок відповідно до інтересів та здібностей студента)</p>
-                  <p class="course-description">Можливість пройти курс з другом, та навіть групою друзів</p>
+                  <p class="course-description">
+                    Онлайн марафон з вивчення англійської мови за найсвіжішою різдвяною новинкою NETFLIX. Най-найсвіжіша прем’єра цього року, а у нас вже розроблений курс до неї 
+                    <span class="emoji fire ml-1"></span>
+                    <span class="emoji fire"></span>
+                    <span class="emoji fire"></span>
+                  </p>
+                  <p class="course-description">
+                    Dash &amp; Lily - романтичний, комедійний, телевізійний серіал.
+                    Прем'єра із 8 серій на Netflix відбулася 10 листопада 2020 року.
+                  </p>
+                  <p class="course-description">
+                    Нью Йорк, Різдво, закоханості, вечірки, розчарування, розставання, гумор
+                  </p>
+                  <p class="course-description">
+                    Це просто mind blowing
+                    <i class="fas fa-grin-hearts ml-1" aria-hidden="true"></i>
+                  </p>
+                  <p class="course-description">
+                      Розпочинаємо 10.01.21, але доєднатися можна і пізніше.
+                  </p>
+                  <p class="course-description">
+                    Проводьте перші дні нового року, не в підготовці до уроків, а насолоджуючись NETFLIX новинкою та різдвяними святами!
+                  </p>
                   <?php include ('free-lesson.php'); ?>
                 </div>
               </div>
               <div class="col-12 col-md-6">
-                <div class="course-left course-left-travel d-none d-md-flex">
-                  
-                </div>
+                <div class="course-left course-left-dash-lily d-none d-md-flex"></div>
               </div>
             </div>
           </div>
--- zteacher_2.0/it-english.php
+++ zteacher_2.0/it-english.php
@@ -12,7 +12,7 @@
     <meta property="og:type" content="article">
     <meta property="og:image" content="img/favicon.png">
     <meta property="og:site_name" content="Zteacher Language School">
-    <title>Travel English - Zteacher Language School</title>
+    <title>IT English - Zteacher Language School</title>
     <body>
 
       <?php include ('trial_lesson_modal.php'); // запись на пробный modal ?>
@@ -25,14 +25,23 @@
               <div class="col-md-6">
                 <div class="course-right d-flex">
                   <h2 class="course-page-title">
-                    IT
+                    Курс для юних IT-шників
                   </h2>
                   <div class="mobile-course-img course-left-it"></div>
-                  <p class="course-description">Позбавимо вас від страху непорозумінь під час подорожей за 10 занять. Розбираємо та тренуємо найбільш важливі теми та знайомимосьз визначними місцями.</p>
-                  <p class="course-description">Судячи з інстаграм, перші ластівки полетіли до вже приймаючих готелів, в межах України. Невдовзі найсміливіші відправляться і в закордонні поїздки. У нас, для плануючих відпочивати із задоволенням, та отримати максимум від подорожей, нереально цікава пропозиція.</p>
-                  <p class="course-description">10 уроків, тривалістю 1.15 год. <br>Найцікавіші факти, про місця 10ти міст світу</p>
-                  <p class="course-description">10 тем, якими найважливіше оволодіти для вільного, впевненого спілкування під час подорожей пропрацювання лексики, конструкцій, сталих виразів. Незважаючи на курс, індивідуальний підхід до кожного студента зберігається (адаптуємо урок відповідно до інтересів та здібностей студента)</p>
-                  <p class="course-description">Можливість пройти курс з другом, та навіть групою друзів</p>
+                  <p class="course-description">Інтерактивний підхід</p>
+                  <p class="course-description">Практика з першого уроку</p>
+                  <p class="course-description">Ознайомлення з базовими принципами програмування англійською</p>
+                  <p class="course-description">Індивідуальний супровід учнів</p>
+                  <p class="course-description">Цікаві домашні завдання</p>
+                  <p class="course-description">
+                    <a href="https://www.linkedin.com/in/dima-shutenko/" class="specialist">
+                          Викладач
+                        </a>
+                        — практикуючий
+                        <span class="one-line">
+                          IT-спеціаліст
+                        </span>
+                  </p>
                   <?php include ('free-lesson.php'); ?>
                 </div>
               </div>
