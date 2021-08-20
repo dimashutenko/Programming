6ced6d1d570aeacf692fe96271f03c1b3d92bf49
Dima Shutenko <dima.shutenko.official@gmail.com>
2021-08-20 14:40:37 +0300

    testimonials mobile

--- zteacher_2.0/testimonials.php
+++ zteacher_2.0/testimonials.php
@@ -2,9 +2,6 @@
 	<h2 class="section-headline">
 		Відгуки
 	</h2>
-	<div class="testimonials-wrapper">
-		<div class="testimonial"></div>
-	</div>
   <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-indicators">
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -25,6 +22,21 @@
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="15" aria-label="Slide 16" ></button>
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="16" aria-label="Slide 17"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="17" aria-label="Slide 18"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="18" aria-label="Slide 19"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="19" aria-label="Slide 20"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="20" aria-label="Slide 21" ></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="21" aria-label="Slide 22"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="22" aria-label="Slide 23"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="23" aria-label="Slide 24"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="24" aria-label="Slide 25"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="25" aria-label="Slide 26" ></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="26" aria-label="Slide 27"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="27" aria-label="Slide 28"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="28" aria-label="Slide 29"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="29" aria-label="Slide 30"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="30" aria-label="Slide 31" ></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="31" aria-label="Slide 32"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="32" aria-label="Slide 33"></button>
     </div>
     <div class="carousel-inner">
       <div class="carousel-item active" data-bs-interval="30000">
@@ -81,6 +93,52 @@
       <div class="carousel-item" data-bs-interval="10000">
         <img src="img/testimonial17.jpg" class="d-block w-100" alt="...">
       </div>
+      <div class="carousel-item" data-bs-interval="10000">
+        <img src="img/testimonial18.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item" data-bs-interval="10000">
+        <img src="img/testimonial19.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item" data-bs-interval="10000">
+        <img src="img/testimonial20.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item" data-bs-interval="10000">
+        <img src="img/testimonial21.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item " data-bs-interval="30000">
+        <img src="img/testimonial22.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item" data-bs-interval="10000">
+        <img src="img/testimonial23.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item" data-bs-interval="10000">
+        <img src="img/testimonial24.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item" data-bs-interval="10000">
+        <img src="img/testimonial25.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item" data-bs-interval="10000">
+        <img src="img/testimonial26.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item" data-bs-interval="10000">
+        <img src="img/testimonial27.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item" data-bs-interval="10000">
+        <img src="img/testimonial28.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item" data-bs-interval="10000">
+        <img src="img/testimonial29.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item" data-bs-interval="10000">
+        <img src="img/testimonial30.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item" data-bs-interval="10000">
+        <img src="img/testimonial31.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item " data-bs-interval="30000">
+        <img src="img/testimonial32.jpg" class="d-block w-100" alt="...">
+      </div>
+
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
