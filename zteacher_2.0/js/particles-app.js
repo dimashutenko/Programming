85471c2b323537ea68c160e029f312cf9218702e
dimashutenko <58172685+dimashutenko@users.noreply.github.com>
2021-09-16 11:45:26 +0300

    pale-violet-header

--- zteacher_2.0/js/particles-app.js
+++ zteacher_2.0/js/particles-app.js
@@ -23,7 +23,7 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#ffffff"
+        "value": "#25D9F1"
       },
       "shape": {
         "type": "circle",
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#ffffff",
+        "color": "#25D9F1",
         "opacity": 0.4,
         "width": 1
       },
